315c02067d697fec7526e814aa9b266bfb3d0efc
yossefdahan <yossefdahan1@gmail.com>
2024-03-06 19:48:13 +0200

    fix issues

--- travel-tip-starter/js/app.controller.js
+++ travel-tip-starter/js/app.controller.js
@@ -278,9 +278,9 @@ function renderLocStats() {
     locService.getLocCountByRateMap().then(stats => {
         handleStats(stats, 'loc-stats-rate')
     })
-    // locService.getLocCountByUpdateMap().then(stats => {
-    //     handleStats(stats, 'loc-stats-update')
-    // })
+    locService.getLocCountByUpdateMap().then(stats => {
+        handleStats(stats, 'loc-stats-update')
+    })
 }
 
 function handleStats(stats, selector) {
